d14a9954d84b20b18017867df42735e10c0989f6
Javier Casas <javcasas@gmail.com>
2018-09-16 16:01:15 -0500

    Progress

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,9 +3,9 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DefaultSignatures #-}
+module Lib where
 
 import Numeric.Natural
 import GHC.Generics
@@ -15,8 +15,7 @@ someFunc = do
     multiStepDescent 1 100 (Example (Parameter 48) 1 1 1)
     print $ extractParameters $ from $ (Example (Parameter 48) 1 1 1)
 
-data Parameter a = Parameter a deriving (Show, Eq)
-unParameter (Parameter x) = x
+newtype Parameter a = Parameter { unParameter :: a } deriving (Show, Eq, Num)
 
 data Example = Example {
     ex :: Parameter Double,
@@ -36,8 +35,8 @@ sweep e = mapM_ p vals
         vals = (\v -> e {ex=Parameter v}) <$> [-10.0, -9.9 .. 10.0]
         p x = print (unParameter $ ex x, fx x)
 
-delta :: Example -> Double
-delta e = ((fx e) - (fx e2))/distance
+delta2 :: Example -> Double
+delta2 e = ((fx e) - (fx e2))/distance
     where
         e2 = e {ex=Parameter $ unParameter (ex e) + distance}
         distance = 0.00001
@@ -45,7 +44,7 @@ delta e = ((fx e) - (fx e2))/distance
 gradientDescent :: Double -> Example -> Example
 gradientDescent increment e = e {ex=Parameter $ unParameter (ex e) + distance}
     where
-        distance = delta e * increment
+        distance = delta2 e * increment
 
 multiStepDescent :: Double -> Int -> Example -> IO ()
 multiStepDescent _ 0 _ = pure ()
@@ -53,12 +52,33 @@ multiStepDescent increment iters e = do
                            pr e
                            multiStepDescent (increment * 0.95) (iters - 1) next
     where
-        pr e = print $ ("x", (unParameter . ex) e, "y", fx e, "dy/dx", delta e)
+        pr e = print $ ("x", (unParameter . ex) e, "y", fx e, "dy/dx", delta2 e)
         next = gradientDescent increment e
+
+newtype Deltas a = Deltas a deriving (Show, Eq)
+class GradientDescent a where
+    type DeltaRep a :: *
+    
+    delta :: a -> DeltaRep a
+    applyDelta :: Double -> DeltaRep a -> a -> a
+    f :: a -> Double
+
+descent :: (GradientDescent a) => Double -> a -> [a]
+descent increment initial = 
+    let d = delta initial in
+    let applied = applyDelta increment d initial in
+    applied : descent (increment * 0.95) applied
+    
 
 class ExtractParameters a where
   -- | Return number of constuctor fields for a value.
   extractParameters :: a -> [Double]
+  default extractParameters :: (Generic a, ExtractParameters (Rep a)) => a -> [Double]
+  extractParameters x = extractParameters $ from x
+
+class ExtractParameters1 f where
+  -- | Return number of constuctor fields for a value.
+  extractParameters1 :: f p -> [Double]
 
 instance ExtractParameters (V1 p) where
   extractParameters _ = []
@@ -81,3 +101,41 @@ instance (ExtractParameters (a p), ExtractParameters (b p)) => ExtractParameters
 
 instance (ExtractParameters (a p), ExtractParameters (b p)) => ExtractParameters ((a :*: b) p) where
   extractParameters (a :*: b) = extractParameters a ++ extractParameters b
+
+
+
+class InjectParameters a where
+  -- | Return number of constuctor fields for a value.
+  injectParameters :: a -> [Double] -> (a, [Double])
+
+instance InjectParameters (V1 p) where
+  injectParameters x p = (x, p)
+
+instance InjectParameters (U1 p) where
+  injectParameters x p = (x, p)
+
+instance InjectParameters (K1 i (Parameter Double) p) where
+  injectParameters (K1 (Parameter x)) (h:t) = (K1 (Parameter h), t)
+
+instance InjectParameters (K1 i Double p) where
+  injectParameters x p = (x, p)
+
+instance InjectParameters (f p) => InjectParameters (M1 i c f p) where
+  injectParameters (M1 x) p = (M1 x1, r)
+    where
+        (x1, r) = injectParameters x p
+
+instance (InjectParameters (a p), InjectParameters (b p)) => InjectParameters ((a :+: b) p) where
+  injectParameters (L1 x) p = (L1 x1, r)
+    where
+        (x1, r) = injectParameters x p
+  injectParameters (R1 x) p = (R1 x1, r)
+    where
+        (x1, r) = injectParameters x p
+
+instance (InjectParameters (a p), InjectParameters (b p)) => InjectParameters ((a :*: b) p) where
+  injectParameters (a :*: b) p = 
+    let (a1, p1) = injectParameters a p in
+    let (b1, r) = injectParameters b p1 in
+    ((a1 :*: b1), r)
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,60 @@
-import Test.Hspec
-import Lib
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+
+import Test.Hspec hiding (Example)
+import Lib hiding (Example(..), fx)
+import GHC.Generics
+
+data Example = Example {
+    ex :: Parameter Double,
+    ea :: Double,
+    eb :: Double,
+    ec :: Double
+  } deriving (Show, Eq, Generic)
+
+fx :: Example -> Double
+fx Example { ex, ea, eb, ec} = ea * x * x + eb * x + ec
+    where
+        x = unParameter ex
+
+getX :: Example -> Double
+getX x = unParameter $ ex x
+
+
+instance GradientDescent Example where
+    type DeltaRep Example = Double
+    delta Example {ex, ea, eb, ec} = ea * 2 * x + eb
+        where
+            x = unParameter ex
+    f = fx
+    applyDelta step dx (Example { ex, ea, eb, ec }) = Example {ea, eb, ec, ex=tt }
+        where
+            tt = Parameter $ (unParameter ex) - (step * dx)
 
 
 main :: IO ()
 main = hspec $ do
     describe "Blah" $ do
         it "bleh" $ do 
-            someFunc
+            -- someFunc
             True `shouldBe` True
+    describe "Example" $ do
+        it "Case 1" $ do
+            delta (Example (Parameter (-0.5)) 1 1 1) `shouldBe` 0
+        it "Case 2" $ do
+            f (Example (Parameter (-0.5)) 1 1 1) `shouldBe` 0.75
+            
+        it "Case 3" $ do
+            getX (last $ take 20 $ descent 1 (Example (Parameter (-4.5)) 1 1 1)) `shouldSatisfy` (\x -> x< (-0.499999999) && x> (-0.50001))
+    describe "ExtractParameters" $ do
+        it "extractParameters" $ do
+            (extractParameters $ from $ (Example (Parameter (-4.5)) 1 1 1)) `shouldBe` [-4.5]
+    describe "injectParameters" $ do
+        it "injectParameters" $ do
+            let (x, l) = injectParameters (from $ (Example (Parameter (-4.5)) 1 1 1)) [6]
+            (to x) `shouldBe` (Example (Parameter (7)) 1 1 1)
+            l `shouldBe` []
